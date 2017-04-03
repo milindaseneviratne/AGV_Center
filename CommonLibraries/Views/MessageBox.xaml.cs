d65b5e35f869383cfcd9a23c701233ed6b53113e
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-04-03 14:02:47 +0700

    Added Yes, No, Retry and Cancel buttons to the Messagebox.
    I will ignore AboutBox for now. Will develop that in the end.

--- CommonLibraries/Views/MessageBox.xaml.cs
+++ CommonLibraries/Views/MessageBox.xaml.cs
@@ -20,14 +20,22 @@ namespace CommonLibraries.Views
     /// </summary>
     public partial class MessageBox : Window
     {
-        public MessageBox()
+        public MessageBox(string message)
         {
             InitializeComponent();
+            this.txtMessage.Text = message;
         }
 
         public MessageBox(Exception ex)
         {
             InitializeComponent();
+            this.Title = ex.Message;
+            this.txtMessage.Text = ex.ToString();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
